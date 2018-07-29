5a7e16876ba5da05ae3f15e3e6d5cdf608e2f353
Alexander Pabón <alex312-@hotmail.com>
2018-07-28 22:33:23 -0500

    Implementación de servicio (JSON) en el portafolio de productos

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  constructor( private http: HttpClient ) {
+    console.log('Servicio de información pagina listo');
+
+    // Leer archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+        console.log(resp);
+        // console.log(resp['email']);
+      });
+  }
+
+}
