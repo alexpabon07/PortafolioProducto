74fc9254881d0285c8e9720b6d008434948344f3
Alexander Pabón <alex312-@hotmail.com>
2018-07-29 10:45:35 -0500

    Solución de bug, en el InfoPaginaService del portafolio de productos

--- src/app/interfaces/info-pagina.interface.ts
+++ src/app/interfaces/info-pagina.interface.ts
@@ -1,10 +1,10 @@
 export interface InfoPagina {
-  titulo: string;
-  email: string;
-  nombre_corto: string;
-  facebook: string;
-  twitter: string;
-  instagram: string;
-  tumblr: string;
-  equipo_trabajo: any[];
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tumblr?: string;
+  equipo_trabajo?: any[];
 }
